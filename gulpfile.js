f9254e61e57a1bac061bda352586b6b12dba4c08
andrii-dolzhenko <andrii.dolzhenko@gmail.com>
2026-05-19 17:41:31 +0300

    fix: include sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,28 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 
 function compileScss() {
   return gulp
     .src('./src/scss/style.scss')
+
+    .pipe(sourcemaps.init()) // ← старт карты
+
     .pipe(sass().on('error', sass.logError))
+
     .pipe(gulp.dest('./assets/css'))
+
     .pipe(cleanCSS())
     .pipe(rename({
       suffix: '.min'
     }))
+
+    .pipe(sourcemaps.write('.')) // ← создать style.css.map
+
     .pipe(gulp.dest('./assets/css'))
+
     .pipe(browserSync.stream());
 }
 
@@ -29,10 +39,8 @@ function watchFiles() {
     compileScss
   );
 
-  gulp.watch('./*.html').on(
-    'change',
-    browserSync.reload
-  );
+  gulp.watch('./*.html')
+    .on('change', browserSync.reload);
 }
 
 exports.default =
